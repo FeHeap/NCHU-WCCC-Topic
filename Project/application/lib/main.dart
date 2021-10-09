f0d7473755904647da67f63fb9aad3de3eecbace
haha39 <60254357+haha39@users.noreply.github.com>
2021-10-09 16:36:01 +0800

    New app interface

--- Project/application/lib/main.dart
+++ Project/application/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Project Practice',
       theme: ThemeData(
-          primarySwatch: Colors.teal
+          primarySwatch: Colors.cyan
       ),
       home: MyHomePage(title: '臺灣社交距離'),
     );
@@ -37,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
           PopupMenuButton<String>(
             onSelected: handleClick,
             itemBuilder: (BuildContext context) {
-              return {'App介紹', '每日接觸狀況','確診者上船隨機ID','注意事項及個資保護說明',
+              return {'App介紹', '每日接觸狀況','確診者上傳隨機ID','注意事項及個資保護說明',
                 '資料權限控制','常見問題','功能提示'}.map((String choice) {
                 return PopupMenuItem<String>(
                   value: choice,
@@ -48,64 +47,79 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Center (
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '與確診者資料比對無接觸',
-              style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.teal
-              ),
-            ),
-            Text(
-              '請繼續保持社交距離',
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.teal
-              ),
-            ),
-
+      body: //Center(
+      /*child: */LayoutBuilder(
+        builder: (context, constraints) => Column(
+          mainAxisSize: MainAxisSize.min,
+          //mainAxisSize: MainAxisSize.max,
+          children: [
             Container(
-                child: Column (
-                  children: [
-                    Container(
-                      width: window.physicalSize.width,
-                      color: Colors.teal,
-                      child: Text('接觸通知功能已開啟'),
+              color: Colors.grey[350],
+              height: constraints.maxHeight * 0.65,
+              width: constraints.maxWidth ,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '與確診者資料比對無接觸\n',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.cyan
                     ),
-                    Text(
-                      '\n接觸通知功能啟用時間1967/2/20 19:58',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
-                      ),
+                    //textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    '請繼續保持社交距離',
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.cyan
                     ),
-                    Text(
-                      '運作時間比例為99%',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
-                      ),
-                    ),
-                    Text(
-                      '上次檢查時間為2021/12/25 10:54',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
-                      ),
-                    ),
-                  ],
-                )
+                    //textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              //color: Color.fromARGB(20, 226, 207, 100),
+              color: Colors.cyan,
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                //mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Text('接觸通知功能已開啟'),
+                  ]
+              ),
+              margin: EdgeInsets.fromLTRB(50, 20, 50, 0),
+            ),
+            Text(
+              '\n接觸通知功能啟用時間1967/2/20 19:58',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black
+              ),
+            ),
+            Text(
+              '運作時間比例為99%',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black
+              ),
+            ),
+            Text(
+              '上次檢查時間為2021/12/25 10:54',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black
+              ),
             ),
           ],
         ),
+        //)
       ),
     );
   }
