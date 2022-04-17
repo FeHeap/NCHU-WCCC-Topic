cb7b942af5e4d50ae35d5d926c983c33e83de01e
Fe <82155803+FeHeap@users.noreply.github.com>
2022-04-17 17:07:03 +0800

    set heroku server to response to APP and try to get the data from heroku postgres

--- Project_final/database/main.py
+++ Project_final/database/main.py
@@ -1,16 +1,156 @@
-# This is a sample Python script.
+import os
+import psycopg2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def ivPasswdTable():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_command = '''
+            CREATE TABLE IF NOT EXISTS ivPasswdTable (
+                index VARCHAR(3),
+                iv VARCHAR(32),
+                passwd VARCHAR(32)
+            );
+    '''
+    cursor.execute(SQL_command)
+
+    f = open('ivPasswdTable.txt', 'r')
+    ivPasswds = [x.strip('\n').split(',') for x in f.readlines()]
+    f.close()
+
+    for ivPasswd in ivPasswds:
+        print(ivPasswd)
+        SQL_command = '''
+                INSERT INTO ivPasswdTable
+                    (index, iv, passwd)
+                    VALUES (%s, %s, %s);
+        '''
+        cursor.execute(SQL_command, ivPasswd)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def UIDTable():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_command = '''
+            CREATE TABLE IF NOT EXISTS UIDTable (
+                phone VARCHAR(15),
+                UID VARCHAR(64)
+            );
+    '''
+    cursor.execute(SQL_command)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
 
 
-# Press the green button in the gutter to run the script.
+def pointAndLinks():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_command = '''
+                CREATE TABLE IF NOT EXISTS pointAndLinks (
+                    key_UID VARCHAR(67),
+                    links_UID VARCHAR(6800)
+                );
+        '''
+    cursor.execute(SQL_command)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+def covidTable():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_command = '''
+                CREATE TABLE IF NOT EXISTS covid (
+                    key_UID VARCHAR(67)
+                );
+        '''
+    cursor.execute(SQL_command)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+def getPointAndLinks():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_select_command = """
+                       SELECT * FROM pointAndLinks;
+                   """
+    cursor.execute(SQL_select_command)
+    data = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    f = open("pointAndLinks.txt", "w")
+    for subdata in data:
+        f.write(f'{subdata[0]},{subdata[1]}\n')
+    f.close()
+
+def getCovid():
+    herokuCLI_command = 'heroku config:get DATABASE_URL -a nchu-cse-wccc-topic-final'
+    DATABASE_URL = os.popen(herokuCLI_command).read()[:-1]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    cursor = conn.cursor()
+
+    SQL_select_command = """
+                       SELECT * FROM covid;
+                   """
+    cursor.execute(SQL_select_command)
+    data = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    f = open("covid.txt", "w")
+    for subdata in data:
+        f.write(f'{subdata}\n')
+    f.close()
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # ivPasswdTable()
+    # UIDTable()
+    # pointAndLinks()
+    # covidTable()
+    getPointAndLinks()
+    getCovid()
--- Project_final/server/app.py
+++ Project_final/server/app.py
@@ -2,13 +2,141 @@ from flask import Flask,render_template,request
 import os
 import psycopg2
 
+import random
+
+from base64 import b64encode, b64decode
+from binascii import unhexlify
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+
+
+def encrypt(iv, passwd, msg):
+    # Convert Hex String to Binary
+    iv = unhexlify(iv)
+    passwd = unhexlify(passwd)
+
+    # Pad to AES Block Size
+    msg = pad(msg.encode(), AES.block_size)
+    # Encipher Text
+    cipher = AES.new(passwd, AES.MODE_CBC, iv)
+    cipher_text = cipher.encrypt(msg)
+
+    # Encode Cipher_text as Base 64 and decode to String
+    out = b64encode(cipher_text).decode('utf-8')
+
+    return out
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/getCipherUID', methods=['POST'])
+def getCipherUID():
+    phone = request.values['phone']
 
+    # Heroku works
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    # connect with database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    # create cursor
+    cursor = conn.cursor()
+
+    SQL_select_command = """
+           SELECT * FROM UIDTable
+           WHERE phone={phone};
+       """
+    # execute SQL
+    cursor.execute(SQL_select_command)
+    data = cursor.fetchall()
+
+    if len(data) != 0:
+        return ''
+    else:
+        hexArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+        UID = ''
+        for j in range(64):
+            UID += hexArray[random.randint(0, 15)]
+        SQL_insert_command = """
+                   INSERT INTO ivPasswdTable
+                    (phone, UID)
+                    VALUES (%s, %s);
+               """
+        # execute SQL
+        cursor.execute(SQL_insert_command, phone, UID)
+
+
+        SQL_select_command = """
+                   SELECT * FROM ivPasswdTable;
+               """
+        cursor.execute(SQL_select_command)
+        ivPasswds = cursor.fetchall()
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        random.shuffle(ivPasswds)
+
+        cipherIDs = ''
+        for ivPasswd in ivPasswds[:96]:
+            cipherIDs += ',' + ivPasswd[0] + encrypt(ivPasswd[1], ivPasswd[2], UID)
+
+        return cipherIDs[1:]
+
+@app.route('/sendPointAndLinks', methods=['POST'])
+def sendPointAndLinks():
+    pointAndLinks = request.values['pointAndLinks']
+
+    # Heroku works
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    # connect with database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    # create cursor
+    cursor = conn.cursor()
+
+    SQL_insert_command = """
+                       INSERT INTO pointAndLinks
+                        (key_UID, links_UID)
+                        VALUES (%s, %s);
+                   """
+    # execute SQL
+    cursor.execute(SQL_insert_command, pointAndLinks[:67], pointAndLinks[68:])
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return ''
+
+@app.route('/sendCovid', methods=['POST'])
+def sendCovid():
+    UID = request.values['UID']
+
+    # Heroku works
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    # connect with database
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    # create cursor
+    cursor = conn.cursor()
+
+    SQL_insert_command = """
+                       INSERT INTO covid
+                        (UID)
+                        VALUES (%s);
+                   """
+    # execute SQL
+    cursor.execute(SQL_insert_command, UID)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return ''
 
 if __name__ == '__main__':
     app.run()
